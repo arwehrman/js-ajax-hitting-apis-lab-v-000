9bdcea61a88e87806904df1e3c1dba623744c827
Amanda Wehrman <arwehrman@gmail.com>
2018-02-06 00:24:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ function displayBranches() {
 };
 
 function displayCommits() {
-
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
 };
