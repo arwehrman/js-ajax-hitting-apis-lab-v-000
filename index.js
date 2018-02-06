8e75100ed8b1540d013283e04f6a04fd0e242cea
Amanda Wehrman <arwehrman@gmail.com>
2018-02-06 00:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function getRepositories() {
   const req = new XMLHttpRequest();
   let username = document.getElementById('username').value
   req.addEventListener('load', displayRepositories)
-  req.open("GET", `https://api.github.com//users/${username}/repos`)
+  req.open("GET", `https://api.github.com/users/${username}/repos`)
 };
